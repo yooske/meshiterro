435ff1fa260a4ee2c976cb5f17a9d61b45f7c734
EC2 Default User <ec2-user@ip-172-31-18-24.ap-northeast-1.compute.internal>
2022-08-10 14:41:16 +0000

    [meshiterro]finish 5-21 バリデーションの設定

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,6 +3,11 @@ class PostImage < ApplicationRecord
   belongs_to :user
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  
+  #shop_nameが存在しているかを確認するバリデーション
+  validates :shop_name, presence: true
+  #imageが存在しているかを確認するバリデーション
+  validates :image, presence: true
 
   def get_image
     unless image.attached?
